7a5af282e4190caa7e28f5ecb8604e44cbf6b171
leesj8116 <leesj8115@skuniv.ac.kr>
2024-12-03 17:07:34 +0900

    Base Entity 작성 (createdDate, lastModifiedDate)

---
+++ src/main/java/devgraft/dgcinemabackend/common/domain/BaseEntity.java
@@ -0,0 +1,22 @@
+package devgraft.dgcinemabackend.common.domain;
+
+import java.time.LocalDateTime;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public class BaseEntity {
+	@CreatedDate
+	protected LocalDateTime createdDate;
+
+	@LastModifiedDate
+	protected LocalDateTime lastModifiedDate;
+}
