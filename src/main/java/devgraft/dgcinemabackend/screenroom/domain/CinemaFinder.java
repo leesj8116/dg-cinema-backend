751a9b0c97a37d5a104c32c5dddf2b0cabcb0955
leesj8116 <leesj8115@skuniv.ac.kr>
2024-12-22 23:06:55 +0900

    🚧 상영관(ScreenRoom)을 극장(Cinema) 패키지 안으로 포함, 코드 정리

--- src/main/java/devgraft/dgcinemabackend/screenroom/domain/CinemaFinder.java
+++
@@ -1,9 +0,0 @@
-package devgraft.dgcinemabackend.screenroom.domain;
-
-import java.util.Optional;
-
-import devgraft.dgcinemabackend.cinema.domain.Cinema;
-
-public interface CinemaFinder {
-	Optional<Cinema> findById(Long id);
-}
