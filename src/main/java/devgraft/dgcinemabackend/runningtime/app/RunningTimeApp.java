e4ec49399809d603c18a89abfde0716c2fb24bbc
leesj8116 <leesj8115@skuniv.ac.kr>
2024-12-26 21:55:23 +0900

    상영시간 조회 메소드 추가 설명 기입

--- src/main/java/devgraft/dgcinemabackend/runningtime/app/RunningTimeApp.java
+++ src/main/java/devgraft/dgcinemabackend/runningtime/app/RunningTimeApp.java
@@ -17,6 +17,12 @@ public class RunningTimeApp {
 	private final RunningTimeRepository runningTimeRepository;
 	private final MovieFinder movieFinder;
 
+	/**
+	 * 영화 제목을 이용하여 상영일정을 조회한다.
+	 * 이 때, 아직 상영시작 전인 영화만 조회한다.
+	 * @param movieTitle
+	 * @return
+	 */
 	public List<RunningTime> getRunningTimesByMovieTitle(String movieTitle) {
 		List<Movie> movies = movieFinder.findMoviesByMovieTitle(movieTitle);
 
