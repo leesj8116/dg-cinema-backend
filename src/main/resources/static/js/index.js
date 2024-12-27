96f8b81d37e8dfa2f2a42930fe89b9aa432b1030
leesj8116 <leesj8115@skuniv.ac.kr>
2024-12-27 15:16:11 +0900

    🎨 테스트 화면 업데이트
    1. 현재까지 선택한 예약 항목 표시
    2. 좌석 선택시 시각적 표현 추가

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -8,13 +8,13 @@ const seatsCol = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12']
 
 document.addEventListener('DOMContentLoaded', async () => {
     if (localStorage.getItem('account') === null) {
-        hideLoginInfoArea()
+        hideLoginInfoArea();
     } else {
-        showLoginInfoArea()
+        showLoginInfoArea();
     }
 
-    await getMovies()
-    await searchRunningTimeByMovietitle()
+    await getMovies();
+    await searchRunningTimeByMovietitle();
 })
 
 /**
@@ -25,11 +25,11 @@ const getMovies = async () => {
     await fetch('/movie')
         .then((response) => response.json())
         .then((json) => {
-            updateMovieTable(json)
+            updateMovieTable(json);
         }).catch((error) => {
-            console.error(error)
-        })
-}
+            console.error(error);
+        });
+};
 
 /**
  * 영화 제목 입력을 통해 영화를 조회한다.
@@ -37,18 +37,18 @@ const getMovies = async () => {
  * @returns {Promise<void>}
  */
 const searchRunningTimeByMovietitle = async () => {
-    const title = document.getElementById('search-movie-title').value
-    const queryString = new URLSearchParams({'title': title}).toString()
+    const title = document.getElementById('search-movie-title').value;
+    const queryString = new URLSearchParams({'title': title}).toString();
 
     await fetch(`/running-time?${queryString}`)
         .then((response) => response.json())
         .then((json) => {
-            console.log('상영시간 정보', json)
-            uploadRunningTimetable(json)
+            console.log('상영시간 정보', json);
+            uploadRunningTimetable(json);
         }).catch((error) => {
-            console.error(error)
-        })
-}
+            console.error(error);
+        });
+};
 
 /**
  * 화면에 표시된 영화 목록을 갱신한다. (도움 - chatGPT4o)
@@ -123,8 +123,8 @@ const uploadRunningTimetable = (runningTimes) => {
         });
 
         // 날짜와 시간을 분리
-        const datePart = dateString.substring(0, 14)
-        const timePart = dateString.substring(14)
+        const datePart = dateString.substring(0, 14);
+        const timePart = dateString.substring(14);
 
         // 날짜 부분을 '년 월 일' 형식으로 변환
         const [year, month, day] = datePart.split('.').map(part => part.trim());
@@ -168,7 +168,10 @@ const uploadRunningTimetable = (runningTimes) => {
             searchSpan.classList.add('past');
         } else {
             // 아직 상영 전이라면 좌석 확인으로 안내
-            searchSpan.setAttribute('onclick', `reservationCheckSeat('${runningTime.runningTimeId}')`);
+            // @TODO: 이게 맞나 싶은 데이터 전달
+            searchSpan.setAttribute('onclick',
+                `reservationCheckSeat('${runningTime.runningTimeId}', '${runningTime.movie.title}',
+                '${dateString}', '${runningTime.screenRoom.cinemaId}', '${runningTime.screenRoom.screenNumber}')`);
         }
         actionCell.appendChild(searchSpan);
 
@@ -186,107 +189,111 @@ const uploadRunningTimetable = (runningTimes) => {
 }
 
 /**
+ * 선택한 상영 시간의 잔여 좌석을 확인한다.
+ * @returns {Promise<void>}
+ */
+const reservationCheckSeat = async (runningTimeId, title, startTime, cinema, screenNumber) => {
+    document.getElementById('reservation-task-what').innerText = title;
+    document.getElementById('reservation-task-when').innerText = startTime;
+    document.getElementById('reservation-task-where').innerText = `${cinema} / ${screenNumber}관`;
+
+    const queryString = new URLSearchParams({'runningTime': runningTimeId}).toString();
+    // 좌석 현황 조회 후 화면에 업로드
+    await fetch(`/reservation/seat?${queryString}`)
+        .then((response) => response.json())
+        .then((json) => {
+            updateReservationSeat(json);
+        }).catch((error) => {
+            console.error(error);
+            alert('잔여 좌석 조회에 실패했습니다. 잠시 후에 다시 시도해주세요.');
+        })
+}
+
+/**
  * '로그인' 버튼 클릭시 로그인을 흉내낸다.
  */
 const loginEvent = async () => {
-    const accountElement = document.getElementById('account-field')
-    const passwordElement = document.getElementById('password-field')
-    const accountValue = accountElement.value
-    const passwordValue = passwordElement.value
+    const accountElement = document.getElementById('account-field');
+    const passwordElement = document.getElementById('password-field');
+    const accountValue = accountElement.value;
+    const passwordValue = passwordElement.value;
 
     // 입력 값 검사
     // Email validation regex pattern   (도움 : chatGPT)
     const emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
 
     if (accountValue !== 'admin' && !emailPattern.test(accountValue)) {
-        alert('계정을 올바르게 입력해주세요')
-        accountElement.focus()
+        alert('계정을 올바르게 입력해주세요');
+        accountElement.focus();
         return;
     }
     if (passwordValue.trim() === '') {
-        alert('비밀번호를 올바르게 입력해주세요')
-        passwordElement.focus()
+        alert('비밀번호를 올바르게 입력해주세요');
+        passwordElement.focus();
         return;
     }
 
     // 전제 조건에 따라, 로그인은 구현된 것으로 가정
-
-    localStorage.setItem('account', accountValue)
+    localStorage.setItem('account', accountValue);
 
     switch (accountValue) {
         case 'admin':
-            localStorage.setItem('nickname', '운영자')
+            localStorage.setItem('nickname', '운영자');
             break;
         case 'user':
-            localStorage.setItem('nickname', '사용자')
+            localStorage.setItem('nickname', '사용자');
             break;
         case 'Pcloud63514@gmail.com':
-            localStorage.setItem('nickname', '권태헌')
+            localStorage.setItem('nickname', '권태헌');
             break;
         case 'leesj8115@gmail.com':
-            localStorage.setItem('nickname', '이승주')
+            localStorage.setItem('nickname', '이승주');
             break;
         default:
-            localStorage.setItem('nickname', '몰?루는 유저')
+            localStorage.setItem('nickname', '몰?루는 유저');
     }
-    showLoginInfoArea()
+    showLoginInfoArea();
 
     // 로그인 처리 후 계정, 비밀번호 필드는 초기화
     accountElement.value = '';
     passwordElement.value = '';
-}
+};
 
 /**
  * '로그아웃' 버튼 클릭시 로그아웃을 흉내낸다.
  */
 const logout = () => {
-    localStorage.clear()
-    hideLoginInfoArea()
+    localStorage.clear();
+    hideLoginInfoArea();
     // 계정 입력 란에 포커스
-    document.getElementById('account-field').focus()
-}
+    document.getElementById('account-field').focus();
+};
 
 /**
  * 로그인 입력 폼 대신 사용자 정보 화면을 보여준다.
  */
 const showLoginInfoArea = () => {
-    document.getElementById('user-name').innerText = localStorage.getItem('nickname')
-    document.getElementById('login-area').style.display = 'none'
-    document.getElementById('login-info-area').style.display = 'block'
-}
+    document.getElementById('user-name').innerText = localStorage.getItem('nickname');
+    document.getElementById('login-area').style.display = 'none';
+    document.getElementById('login-info-area').style.display = 'block';
+};
 
 /**
  * 로그인 입력 폼을 보여주고, 사용자 정보 화면을 숨긴다.
  */
 const hideLoginInfoArea = () => {
-    document.getElementById('login-area').style.display = 'block'
-    document.getElementById('login-info-area').style.display = 'none'
-}
+    document.getElementById('login-area').style.display = 'block';
+    document.getElementById('login-info-area').style.display = 'none';
+};
 
 /**
  * '검색' 버튼을 클릭시, 해당 줄의 영화 제목을 검색창으로 전달한다.
  */
 const passByMovieTitle = (movieName) => {
-    document.getElementById('search-movie-title').value = movieName
-    document.getElementById('search-movie-title-btn').focus()
-}
+    document.getElementById('search-movie-title').value = movieName;
+    document.getElementById('search-movie-title-btn').focus();
+};
 
-
-/**
- * 선택한 상영 시간의 잔여 좌석을 확인한다.
- * @returns {Promise<void>}
- */
-const reservationCheckSeat = async (runningTimeId) => {
-    const queryString = new URLSearchParams({'runningTime': runningTimeId}).toString()
-
-    await fetch(`/reservation/seat?${queryString}`)
-        .then((response) => response.json())
-        .then((json) => {
-            updateReservationSeat(json)
-        }).catch((error) => {
-            console.error(error)
-        })
-}
 
 /**
  * 예약 좌석 현황을 그려준다
@@ -305,19 +312,38 @@ const updateReservationSeat = (seats) => {
         const row = document.createElement('tr');
 
         seatsCol.forEach(c => {
-            const td = document.createElement('td');
-            td.id = `${r}${c}`
-            td.innerText = `${r}${c}`
+            const seatNo = `${r}${c}`;
 
-            if (seats.indexOf(`${r}${c}`) !== -1) {
-                td.classList.add('already') // 예약된 자리는 다르게 표시
+            const td = document.createElement('td');
+            td.id = seatNo;
+            td.innerText = seatNo;
+
+            td.setAttribute('onclick', `selectMySeat('${seatNo}')`);
+
+            if (seats.indexOf(seatNo) !== -1) {
+                td.classList.add('already'); // 예약된 자리는 다르게 표시
             }
 
-            row.appendChild(td)
+            row.appendChild(td);
         })
 
-        tableBody.appendChild(row)
-    })
+        tableBody.appendChild(row);
+    });
+};
 
+/**
+ * 선택한 좌석을 '선택 내용' 폼에 전달한다.
+ * @param mySeat
+ */
+const selectMySeat = (mySeat) => {
+    const selectedList = document.getElementsByClassName('selected');
 
-}
+    // 기존에 선택된 항목은 제거
+    for (let i = 0; i < selectedList.length; i++) {
+        selectedList.item(i).classList.remove('selected');
+    }
+
+    document.getElementById('seat-no').value = mySeat;
+    document.getElementById(mySeat).classList.add('selected');
+    document.getElementById('regist-reservation-btn').focus();  // 예약 버튼에 포커스
+};
