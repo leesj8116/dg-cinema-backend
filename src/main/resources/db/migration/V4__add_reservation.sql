90d3964667a7eb083b30da7c94a6af50e5ea8094
leesj8116 <leesj8115@skuniv.ac.kr>
2025-01-02 10:33:37 +0900

    🧑‍💻 예약 좌석 검사를 위한 상영시간 데이터 시간 변경
    2025년 새해가 밝았습니다

--- src/main/resources/db/migration/V4__add_reservation.sql
+++ src/main/resources/db/migration/V4__add_reservation.sql
@@ -5,7 +5,7 @@
 -- 50번째 상영 시간 데이터 추가
 INSERT INTO running_time (movie_id, cinema_id, screen_number, start_time)
 VALUES
-    (4, 1, 1, TIMESTAMP '2024-12-31 18:00:00');
+    (4, 1, 1, TIMESTAMP '2025-12-31 18:00:00');
 
 -- 50번째에 대해 예약 데이터 사전 등록
 INSERT INTO reservation (user_id, running_time_id, seet_no)
